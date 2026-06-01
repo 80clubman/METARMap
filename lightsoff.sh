d21a430087fe638c61272d2c1caa5ae846516df2
80clubman <jdfrye6@gmail.com>
2026-05-31 21:37:34 -0500

    Update lightsoff.sh

--- lightsoff.sh
+++ lightsoff.sh
@@ -1,3 +1,3 @@
-/usr/bin/sudo pkill -F /home/pi/offpid.pid
-/usr/bin/sudo pkill -F /home/pi/metarpid.pid
-/usr/bin/sudo /usr/bin/python3 /home/pi/pixelsoff.py & echo $! > /home/pi/offpid.pid
+/usr/bin/sudo pkill -F /home/pimetar/offpid.pid
+/usr/bin/sudo pkill -F /home/pimetar/metarpid.pid
+/usr/bin/sudo /usr/bin/python3 /home/pimetar/METARMap/pixelsoff.py & echo $! > /home/pi/offpid.pid
